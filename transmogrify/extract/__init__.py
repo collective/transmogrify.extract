c26f7a0b7477c4bd0f74fd930df84f65d6db6c87
Alex Clark <aclark@aclark.net>
2011-04-21 15:11:38 -0400

    Add boilerplate

--- transmogrify/extract/__init__.py
+++ transmogrify/extract/__init__.py
@@ -1,4 +1,14 @@
 
-class Extract(object):
-    pass
+from collective.transmogrifier.interfaces import ISectionBlueprint, ISection
+from zope.interface import classProvides, implements
 
+class Extract(object):
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+
+    def __iter__(self):
+        for item in self.previous:
+            yield item
