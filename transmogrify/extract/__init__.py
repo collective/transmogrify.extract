15966b5f94c10ab0dc77bc26c0d25e2cdea9c9b2
Alex Clark <aclark@aclark.net>
2011-04-21 16:38:37 -0400

    Punt

--- transmogrify/extract/__init__.py
+++ transmogrify/extract/__init__.py
@@ -1,4 +1,6 @@
 
+import lxml
+
 from collective.transmogrifier.interfaces import ISectionBlueprint, ISection
 from zope.interface import classProvides, implements
 
@@ -12,3 +14,23 @@ class Extract(object):
     def __iter__(self):
         for item in self.previous:
             yield item
+
+
+#            root = lxml.html.fromstring(item['text'])
+#            text = """<div id="content">"""
+#            for tag in [i for i in root.iter()]:
+#                tag_id = tag.get('id')
+#                if tag_id is not None:
+#                    if tag.get('id') == 'content':
+#                        children = tag.getchildren()
+#                        for child in children:
+#                            if child.text is not None:
+#                                try:
+#                                    text += '<%s>%s</%s>' % (
+#                                        child.tag, child.text.decode('utf-8'), child.tag)
+#                                except:
+#                                    pass
+#                        
+#            text += """</div>"""
+#            item['text'] = text
+#            yield item
