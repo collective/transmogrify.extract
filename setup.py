4d6427b247a365c7988ec412603f2e52c353e231
Alex Clark <aclark@aclark.net>
2011-04-21 14:40:56 -0400

    Add entry point and files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='transmogrify.extract'
+    name='transmogrify.extract',
     entry_points = """
     [collective.transmogrifier]
     transmogrify.extract = transmogrify.extract
