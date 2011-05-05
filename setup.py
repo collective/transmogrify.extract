bcfc706cdc2e295795a8e07f313d711c58f392f9
Alex Clark <aclark@aclark.net>
2011-05-05 14:49:05 -0400

    Require lxml

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='transmogrify.extract',
+    install_requires='lxml',
     entry_points = """
     [collective.transmogrifier]
     transmogrify.extract = transmogrify.extract
